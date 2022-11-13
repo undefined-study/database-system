2c9113a3de927f9d94020e86808a96b2fe9b2e3d
안예인 <54733637+ahnanne@users.noreply.github.com>
2022-11-14 00:50:46 +0900

    [A] 5주차 5장 내용 정리

--- week03/prac6.c
+++ week03/prac6.c
@@ -15,4 +15,23 @@ int main()
 
   printf("3.76시간은 %d시간 %d분 %d초입니다.\n", hour, min, sec);
   // 3.76시간은 3시간 45분 35초입니다.
+
+  int score = 70;
+  char grade;
+
+  if (score <= 40)
+  {
+    grade = 'F';
+  }
+  else if ((score > 40) && (score <= 70))
+  {
+    grade = 'B';
+  }
+  else
+  {
+    grade = 'A';
+  }
+
+  printf("당신의 등급은 %c입니다..", grade);
+  // 당신의 등급은 B입니다..
 }
